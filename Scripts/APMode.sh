e86b818cc5082a1c269b34cc179699a3421692eb
Anthony Guella <anthony.guella@gmail.com>
2018-07-06 14:12:23 -0400

    Update DHCPCD.conf

--- Scripts/APMode.sh
+++ Scripts/APMode.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 arg=$1
+DIR=/etc/dhcpcd.conf
 
 if [[ "$arg" == "start" ]]; then
-    echo Starting Access Point
+    echo 'Starting Access Point'
     sudo systemctl start hostapd.service && sudo systemctl start dnsmasq.service
+    echo 'nohook wpa_supplicant' >> DIR
+    echo 'interface wlan0' >> DIR
+    echo 'static ip_address=192.168.50.10/24' >> DIR
+    echo 'static routers=192.168.50.1' >> DIR
+
 elif [[ "$arg" == "stop" ]]; then
-    echo Stopping Access Point
+    echo 'Stopping Access Point'
     sudo systemctl stop hostapd.service && sudo systemctl stop dnsmasq.service
+    sudo head -n -4 /etc/dhcpcd.conf > temp.txt ; sudo mv temp.txt /etc/dhcpcd.conf
 else
-    echo Invalid Argument
+    echo 'Invalid Argument'
 fi
