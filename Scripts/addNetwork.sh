d6041dc16043686ae884cd19f669277f052fb11f
Anthony Guella <anthony.guella@gmail.com>
2018-07-11 16:54:25 -0400

    Add addNetwork

--- Scripts/addNetwork.sh
+++ Scripts/addNetwork.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 DIR=/etc/wpa_supplicant/wpa_supplicant.conf
-SSID=$1
-PASSWORD=$2
+SSID=$(eval echo $1)
+PASSWORD=$(eval echo $2)
 
 echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev" > $DIR
 echo "update_config=1" >> $DIR
 echo "network={" >> $DIR
-echo "  ssid=\"$SSID\"" >> $DIR
+echo "  ssid=\""$SSID"\"" >> $DIR
 if [[ "$PASSWORD" != "" ]]; then
-    echo "  psk=\"$PASSWORD\"" >> $DIR
+    echo "  psk=\""$PASSWORD"\"" >> $DIR
 else
     echo "  key_mgmt=NONE" >> $DIR
 fi
