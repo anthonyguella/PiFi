96d060b8b950f2acf2ef0b6efa802f9a132f9a95
Anthony Guella <anthony.guella@gmail.com>
2018-07-18 15:12:11 -0400

    Update Config Location

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ sudo systemctl disable hostapd
 sudo systemctl disable dnsmasq
 
 # Hostapd Configuration
-sudo cp ./Config/Host/hostapd.conf /etc/hostapd/hostapd.conf
-sudo cp ./Config/Host/hostapd /etc/default/hostapd
+sudo cp ./Config/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ./Config/hostapd /etc/default/hostapd
 
 # DNSmasq Configuration
-sudo cp ./Config/Host/dnsmasq.conf /etc/dnsmasq.conf
+sudo cp ./Config/dnsmasq.conf /etc/dnsmasq.conf
 
 # Webserver Setup
 sudo cp ./Scripts/hotspotWebserver.py /usr/bin/hotspotWebserver.py
@@ -26,4 +26,4 @@ sudo cp ./Scripts/addNetwork.sh /usr/bin/addNetwork.sh
 # APModeService Setup
 sudo cp ./Scripts/autohotspot.service /etc/systemd/system/autohotspot.service
 sudo cp ./Scripts/autohotspotN /usr/bin/autohotspotN
-sudo systemctl enable autohotspot.service   
+sudo systemctl enable autohotspot.service
